fc40c7df6f16f3f19cf501acbfd61b1e7fda8472
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-05-10 16:43:06 +0300

    Added Jumbotron, Accordion, Footer components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FaqsContainer } from './containers/faqs';
+import { FooterContainer } from './containers/footer';
+import { JumbotronContainer } from './containers/jumbotron';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
   );
 }
 
-export default App;
+
